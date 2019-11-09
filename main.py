40e17c6241c4b01bb02da1fa6df8b1c902b1a875
Pakkapon Phongthawee <me@pureapp.in.th>
2019-11-09 14:36:26 +0700

    save

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ compute = lambda v: {
     'b': -v['alpha']-2*v['beta']+v['gamma'],
     'c': v['alpha']-3*v['gamma']/v['beta']
 }
-experiment = SQLiteExperiment(['alpha','beta','gamma'],['a','b','c'],computeFunction=compute,overwrite=True).build()
+experiment = SQLiteExperiment(['alpha','beta','gamma'],['a','b','c'],computeFunction=compute,overwrite=True)
+experiment.build()
 experiment.add('change alpha','alpha',{'alpha':1,'beta':1,'gamma':1},0.25,10,'study to how alpha value affact the output a,b,c')
 experiment.add('varie beta','beta',{'alpha':1,'beta':1,'gamma':1},0.25,10,'How beta variable change output a,b,c ')
 experiment.add('gamma observe','gamma',{'alpha':1,'beta':1,'gamma':1},0.25,10,'output a,b,c that create from difference gamma value')
